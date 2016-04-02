d39dcdc7c5b36434f23bb856b8dadfcd5fae9ef6
kingsley <chukwumakingsley1@gmail.com>
2016-04-02 20:11:11 +0200

    about_methods

--- test.rb
+++ test.rb
@@ -1,7 +1,4 @@
-def test_here_documents_can_also_handle_multiple_lines
-    long_string = <<EOS
-It was the best of times,
-It was the worst of times.
-EOS
+def test(*arg)
+  puts "here is #{arg}"
 end
-puts test_here_documents_can_also_handle_multiple_lines
+puts test("king", 32, 59, 43)
