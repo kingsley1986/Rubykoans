7899bf9c222690974e1ab65003f2223b4068afb3
kingsley <chukwumakingsley1@gmail.com>
2016-04-03 11:33:39 +0200

    about triagle project

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,55 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  case [a,b,c].uniq.size
+  when 1 then :equilateral
+  when 2 then :isosceles
+  else        :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+  #OR
+
+  #[:equilateral, :isosceles, :scalene].fetch([a,b,c].uniq.size - 1)
+
+  #OR
+  #with error message
+#   def triangle(a, b, c)
+#   a, b, c = [a, b, c].sort
+#   raise TriangleError if a <= 0 or a + b <= c
+#   return :equilateral if a == c
+#   return :isosceles if a == b or b == c
+#   return :scalene
+# end
+
+#OR
+
+# def triangle(a, b, c)
+#   if a == b && a == c              # associativity => only 2 checks are necessary
+#     :equilateral
+#   elsif a == b || a == c || b == c # == operator has the highest priority
+#     :isosceles
+#   else
+#     :scalene                       # no need for return keyword
+#   end
+# end
+
+#OR
+
+# class TriangleError < StandardError
+# end
+
+# def triangle(a, b, c)
+#   sides = [a,b,c].sort
+
+#   raise TriangleError if sides.first <= 0 || sides[2] >= sides[1] + sides[0]
+#   return :equilateral if sides.uniq.length  == 1
+#   return :isosceles if sides.uniq.length  == 2
+#   :scalene
+# end
+
+
