733c7fd4d02a6e35e42f15112b780e950f89a818
kingsley <chukwumakingsley1@gmail.com>
2016-04-03 10:41:58 +0200

    about control statement

--- test.rb
+++ test.rb
@@ -1,4 +1,7 @@
-def test(*arg)
-  puts "here is #{arg}"
-end
-puts test("king", 32, 59, 43)
+def test_unless_statement_evaluate_true
+    result = :default_value
+    unless true    # same as saying 'if !true', which evaluates as 'if false'
+      result = :true_value
+    end
+    end
+  puts test_unless_statement_evaluate_true
